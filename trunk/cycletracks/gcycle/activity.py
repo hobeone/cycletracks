1f29ce85e79746191374ce49b362f29c998fcb01
hobe <hobe@8aaa7668-e253-0410-9327-e7e5c7261288>
2008-08-07 21:12:40 +0000

    add username to top of the page move upload template to gcycle/templates/ shrink top grey shaded gif

--- trunk/cycletracks/gcycle/activity.py
+++ trunk/cycletracks/gcycle/activity.py
@@ -4,13 +4,16 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from gcycle.models import Activity, Lap
+from gcycle import views
 
 def show(request, activity):
+  user = views.get_user()
   a = Activity.get(activity)
   points = []
   for lap in a.lap_set:
     points.extend(lap.geo_points.split('\n'))
   return render_to_response('activity/show.html',
       {'activity' : a,
+       'user' : user,
        'points' : points[:-2],
        'centerpoint' : points[int(len(points) / 2.0)]})

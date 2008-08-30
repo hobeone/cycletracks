2e90a16b4e39678ad208856302fe983835042891
hobe <hobe@8aaa7668-e253-0410-9327-e7e5c7261288>
2008-08-30 00:17:00 +0000

    add admin code to see user usage

---
+++ trunk/cycletracks/gcycle/admin.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render_to_response
+from django.contrib.auth import decorators as auth_decorators
+from google.appengine.ext import db
+from google.appengine.api import memcache
+from google.appengine.api import users
+
+from gcycle.models import *
+
+@auth_decorators.login_required
+def users(request):
+  user = request.user
+  allusers = User.all()
+
+  return render_to_response('admin/users.html', {'user': user, 'users': allusers})

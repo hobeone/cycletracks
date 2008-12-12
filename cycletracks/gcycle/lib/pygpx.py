c2a94bf48f44be3975104073a4c859351b644379
hobe <hobe@8aaa7668-e253-0410-9327-e7e5c7261288>
2008-12-12 22:14:42 +0000

    Smooth GPX max speeds Weight speeds by duration for the average speed calculation Add support for multiple laps in a gpx Infer stopped time based on speed Move some logic out of the model into the parser to better support laps add a source_type field to activities so we know where the activity came from (gpx or tcx)

--- cycletracks/gcycle/lib/pygpx.py
+++ cycletracks/gcycle/lib/pygpx.py
@@ -5,6 +5,7 @@ import string
 import math
 import datetime
 import StringIO
+import md5
 
 from gcycle.lib import glineenc
 from gcycle.lib import pytcx
@@ -17,9 +18,12 @@ mainNS=string.Template("{http://www.topografix.com/GPX/1/0}$tag")
 wptTag=mainNS.substitute(tag="trkpt")
 timeTag=mainNS.substitute(tag="time")
 elevationTag=mainNS.substitute(tag="ele")
+segmentTag=mainNS.substitute(tag="trkseg")
 
 
 EARTH_RADIUS = 6371 # km
+
+PAUSED_MIN_SPEED = 1.34112 # meters per second
 
 def parse_zulu(s):
     return datetime.datetime(int(s[0:4]), int(s[5:7]), int(s[8:10]),
@@ -27,13 +31,15 @@ def parse_zulu(s):
 
 # http://www.movable-type.co.uk/scripts/latlong.html
 def calculate_distance(start_lat, start_long, end_lat, end_long):
-  dLat = math.radians(end_lat - start_lat)
-  dLon = math.radians(end_long - start_long)
-  a = (math.sin(dLat/2.0) * math.sin(dLat/2.0) +
-      math.cos(math.radians(start_lat)) * math.cos(math.radians(end_lat)) *
-      math.sin(dLon/2.0) * math.sin(dLon/2.0))
-  c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-  d = EARTH_RADIUS * c
+
+  d = math.acos(
+        math.sin(math.radians(start_lat)) *
+        math.sin(math.radians(end_lat)) +
+        math.cos(math.radians(start_lat)) *
+        math.cos(math.radians(end_lat)) *
+        math.cos(math.radians(end_long - start_long))
+      ) * EARTH_RADIUS;
+
   return d * 1000
 
 def calculate_speed(start_time, start_distance, end_time, end_distance):
@@ -41,26 +47,31 @@ def calculate_speed(start_time, start_distance, end_time, end_distance):
   if tdelta == 0: return 0
   return (end_distance - start_distance) / tdelta
 
-# Assume one activity/lap per file:
-def parse_gpx(filedata):
-  et=ET.parse(StringIO.StringIO(filedata))
-
+def parse_segment(segment, starting_dist = 0):
   time_points = []
+  delta_time = []
   geo_points = []
   altitude_list = []
   distance_list = []
   speed_list = []
+  paused_time = 0
 
   # TODO: error on no time data
   # record time as total delta since start in seconds
 
   start_time = None
-  for wpt in et.findall("//"+wptTag):
+  prev_time = None
+  for wpt in segment.findall(wptTag):
     geo_points.append([float(wpt.get("lat")),float(wpt.get("lon"))])
     waypoint_time = parse_zulu(wpt.findtext(timeTag))
     if start_time is None:
       start_time = waypoint_time
     time_points.append((waypoint_time - start_time).seconds)
+    if not delta_time:
+      delta_time.append(0)
+    else:
+      delta_time.append((waypoint_time - prev_time).seconds)
+    prev_time = waypoint_time
     altitude_list.append(float(wpt.findtext(elevationTag)))
     if len(geo_points) > 1:
       distance_list.append(
@@ -74,8 +85,12 @@ def parse_gpx(filedata):
       )
       speed_list.append(calculate_speed(time_points[-2], distance_list[-2],
           time_points[-1], distance_list[-1]))
+      if speed_list[-1] < PAUSED_MIN_SPEED:
+        paused_time += time_points[-1] - time_points[-2]
+        distance_list[-1] = distance_list[-2]
+        speed_list[-1] = 0.0
     else:
-      distance_list.append(0)
+      distance_list.append(starting_dist)
       speed_list.append(0)
 
   total_ascent = 0.0
@@ -89,16 +104,21 @@ def parse_gpx(filedata):
     else:
       total_descent += altitude_delta * -1
     prev_altitude = i
-  total_meters = sum(distance_list)
+  total_meters = distance_list[-1]
   total_time = time_points[-1]
 
+  # Weight the speeds by how long we were at that speed
+  avg = sum([ row[0] * row[1] for row in map(None,speed_list, delta_time)])
+  average_speed = avg / sum(delta_time) * 3.6
+
+  rolling_time = total_time - paused_time
   lap_record = {
-    'total_meters' : total_meters,
-    'total_rolling_time_seconds' : total_time,
-    'starttime': start_time,
-    'maximum_speed': max(speed_list) * 3.6,
-    'average_speed': total_meters / total_time * 3.6,  # kph,
+    'total_meters' : total_meters - starting_dist,
     'total_time_seconds': total_time,
+    'total_rolling_time_seconds' : rolling_time,
+    'starttime': start_time,
+    'maximum_speed': max(movingAverage3(speed_list)),
+    'average_speed': average_speed,
     'endtime': start_time + datetime.timedelta(seconds=time_points[-1]),
     'geo_points' : geo_points,
     'speed_list' : [ '%.2f' % s for s in speed_list],
@@ -109,3 +129,49 @@ def parse_gpx(filedata):
     'total_descent' : total_descent
     }
   return lap_record
+
+def parse_gpx(filedata):
+  et=ET.parse(StringIO.StringIO(filedata))
+
+  lap_records = []
+  dist = 0
+  for lap in et.findall("//"+segmentTag):
+    lap_records.append(parse_segment(lap, starting_dist = dist))
+    dist = lap_records[-1]['total_meters']
+
+  if not lap_records:
+    raise InvalidTCXFormat(
+      "Activities must have at least 1 lap over 10 meters or 2 minutes.")
+
+  encoded_activity = pytcx.encode_activity_points(
+      [l['geo_points'] for l in lap_records])
+  total_meters = sum([l['total_meters'] for l in lap_records])
+  total_time = sum([l['total_time_seconds'] for l in lap_records])
+
+  average_speeds = [l['average_speed'] for l in lap_records]
+  times = [l['total_rolling_time_seconds'] for l in lap_records]
+
+  avg = sum([ row[0] * row[1] for row in map(None, average_speeds, times)])
+  average_speed = avg / sum(times)
+
+  rolling_time = sum(times)
+
+  activity_record = {
+      'name': '%s' % (lap_records[0]['starttime']),
+      'total_meters': total_meters,
+      'start_time': lap_records[0]['starttime'],
+      'end_time': lap_records[-1]['endtime'],
+      'total_time': total_time,
+      'rolling_time': rolling_time,
+      'average_speed': average_speed,
+      'maximum_speed': max([l['maximum_speed'] for l in lap_records]),
+      'total_ascent': sum([l['total_ascent'] for l in lap_records]),
+      'total_descent': sum([l['total_descent'] for l in lap_records]),
+      'laps': lap_records,
+      'source_hash': md5.new(filedata).hexdigest(),
+      'tcxdata': filedata
+  }
+  activity_record.update(encoded_activity)
+
+  return activity_record
+
